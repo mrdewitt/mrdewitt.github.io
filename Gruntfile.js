2ced681e3b85565f750d7c7797615882b7e56646
mrdewitt <mrdewitt@users.noreply.github.com>
2016-07-11 15:33:57 -0700

    grunt + vulcanize + sw

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    vulcanize: {
+      default: {
+        options: { },
+        files: { 'index.html': 'main.html' },
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-vulcanize');
+
+  // Default task(s).
+  grunt.registerTask('default', ['vulcanize']);
+
+};
